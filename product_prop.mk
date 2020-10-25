1e0a3bdba6b468aebe3de7ff7216650f27113cd9
Mango <mango@gmail.com>
2020-10-25 22:53:01 +0200

    add blur effects from 11

--- product_prop.mk
+++ product_prop.mk
@@ -44,7 +44,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 #PRODUCT_PRODUCT_PROPERTIES += \
 #    ro.bluetooth.library_name=libbluetooth.so
 
- Enable blurs
+# Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
